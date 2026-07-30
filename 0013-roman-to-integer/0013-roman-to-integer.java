141694ad982c249e7f5ac0be90e9bd6cefa80a47
Sania Saha <sahasania2005@gmail.com>
2026-07-31 00:10:59 +0530

    Time: 3 ms (78.04%), Space: 46.2 MB (93.94%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int getValue(char ch){
+        switch(ch){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return 0;
+    }
+    public int romanToInt(String s) {
+        int sum =0;
+        for(int i=0; i<s.length(); i++){
+
+            int current = getValue(s.charAt(i));
+            if(i<s.length() -1){
+                int next = getValue(s.charAt(i+1));
+
+                if(current <next){
+                    sum -= current;
+                }else{
+                    sum += current;
+                }
+            }else{
+                sum += current;
+            }
+        }
+        return sum;
+    }
+}
